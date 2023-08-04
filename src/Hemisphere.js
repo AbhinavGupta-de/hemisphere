268a7ef8cc2a3d006caf36c00091f6c193809504
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-08-04 22:41:20 +0530

    started the hemisphere project

---
+++ src/Hemisphere.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hemisphere = () => {
+	return (
+		<div>
+			<h1>Hi, there, test successful</h1>
+		</div>
+	);
+};
+
+export default Hemisphere;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import Hemisphere from './Hemisphere';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+	window.navigator.geolocation.getCurrentPosition(
+		(position) => console.log(position),
+		(error) => console.log(error)
+	);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	return (
+		<div>
+			<h1>Hi, there, test successful</h1>
+			<Hemisphere />
+		</div>
+	);
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
